a99f32af3062a6b1d208ec6b51944b20555551ea
Ivan Repiso Camara <irepisocamara@gmail.com>
2022-10-16 16:15:47 +0200

    Manejo de errores en service

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,36 +1,56 @@
 const { v4: uuid } = require("uuid");
 const Product = require("../database/Product");
 
-const getAllProducts = () => { 
-    const allProducts = Product.getAllProducts();
-    return allProducts;
+const getAllProducts = () => {
+    try {
+        const allProducts = Product.getAllProducts();
+        return allProducts;
+    } catch (error) {
+        throw error;
+    }
 };
-const getOneProduct = (productId) => { 
-    const product = Product.getOneProduct(productId);
-    return product;
+const getOneProduct = (productId) => {
+    try {
+        const product = Product.getOneProduct(productId);
+        return product;
+    } catch (error) {
+        throw error;
+    }
 };
-const createNewProduct = (newProduct) => { 
+const createNewProduct = (newProduct) => {
     const productToInsert = {
         ...newProduct,
         id: uuid()
     };
 
-    const createdProduct = Product.createNewProduct(productToInsert);
-    return createdProduct;
+    try {
+        const createdProduct = Product.createNewProduct(productToInsert);
+        return createdProduct;
+    } catch (error) {
+        throw error;
+    }
 };
-const updateOneProduct = (productId, changes) => { 
-    const updatedProduct = Product.updateOneProduct(productId, changes);
-    return updatedProduct;
+const updateOneProduct = (productId, changes) => {
+    try {
+        const updatedProduct = Product.updateOneProduct(productId, changes);
+        return updatedProduct;
+    } catch (error) {
+        throw error;
+    }
 };
-const deleteOneProduct = (productId) => { 
-    const deletedProduct = Product.deleteOneProduct(productId);
-    return deletedProduct;
+const deleteOneProduct = (productId) => {
+    try {
+        const deletedProduct = Product.deleteOneProduct(productId);
+        return deletedProduct;
+    } catch (error) {
+        throw error;
+    }
 };
 
 module.exports = {
     getAllProducts,
-    getOneProduct,  
-    createNewProduct, 
-    updateOneProduct, 
+    getOneProduct,
+    createNewProduct,
+    updateOneProduct,
     deleteOneProduct
 };
